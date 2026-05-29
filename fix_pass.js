4632a54bb8cbedaf2e8542d9059ff2cd499edf42
Guilherme <guinunes0817@gmail.com>
2026-05-29 18:53:20 -0300

    fv ghugdfgzdfgsdfg

---
+++ fix_pass.js
@@ -0,0 +1,10 @@
+const pool = require('./node-api/src/db.js');
+const bcrypt = require('bcryptjs');
+
+async function fix() {
+    const hash = await bcrypt.hash('123456', 10);
+    await pool.execute('UPDATE entregadores SET hash_senha = ? WHERE email = ?', [hash, 'gui@gmail.com']);
+    console.log('Senha atualizada com sucesso!');
+    process.exit(0);
+}
+fix();
